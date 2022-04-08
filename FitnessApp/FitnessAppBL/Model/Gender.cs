bd94f06d07cd207b86586a08b9aaf90eeb0f3e8e
Val16eria <85391921+Val16eria@users.noreply.github.com>
2022-04-08 23:53:03 +0300

    Добавьте файлы проекта.

---
+++ FitnessApp/FitnessAppBL/Model/Gender.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace FitnessAppBL.Model
+{
+    /// <summary>
+    /// Пол.
+    /// </summary>
+    public class Gender
+    {
+        /// <summary>
+        /// Название.
+        /// </summary>
+        public string Name { get; }
+        /// <summary>
+        /// Создать новый пол.
+        /// </summary>
+        /// <param name="name"> Имя пола. </param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public Gender(string name)
+        {
+            if(string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Название пола не может быть пустым или null", nameof(name));
+            }
+
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ FitnessApp/FitnessAppBL/Model/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace FitnessAppBL.Model
+{
+    public class User
+    {
+        public string Name { get; }
+
+        public Gender Gender { get; }
+
+        public DateTime BirthDate { get; }
+
+        public double Weight { get; set; }
+
+        public double Height { get; set; }
+
+        public User(string name,
+               Gender gender,
+               DateTime birthDate,
+               double weight,
+               double height)
+        {
+
+        }
+    }
+}
