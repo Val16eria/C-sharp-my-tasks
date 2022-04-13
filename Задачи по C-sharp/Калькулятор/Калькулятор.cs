cffdc14674f0501b7be0aa4bd3178f0a46e818df
Val16eria <85391921+Val16eria@users.noreply.github.com>
2022-04-13 22:52:22 +0300

    Sharp

---
+++ Задачи по C-sharp/Калькулятор/Калькулятор.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Калькулятор
+{
+    internal class Калькулятор
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                Console.Clear();
+                double firstValue, secondValue;
+                string action;
+
+                try
+                {
+                    Console.Write("Введите первое число:  ");
+                    firstValue = double.Parse(Console.ReadLine());
+
+                    Console.Write("Введите второе число:  ");
+                    secondValue = double.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Неверный формат ввода!");
+                    Console.ReadLine();
+                    continue;
+                }
+
+                Console.Write("Выберите операцию:  '+' '-' '*' '/'   ");
+                action = Console.ReadLine();
+
+                switch (action)
+                {
+                    case "+":
+                        Console.Write($"Результат:  {firstValue + secondValue}");
+                        break;
+                    case "-":
+                        Console.Write($"Результат:  {firstValue - secondValue}");
+                        break;
+                    case "*":
+                        Console.Write($"Результат:  {firstValue * secondValue}");
+                        break;
+                    case "/":
+                        if (secondValue == 0)
+                            Console.WriteLine("На ноль делить нельзя!");
+                        else
+                            Console.Write($"Результат:  {firstValue / secondValue}");
+                        break;
+                    default:
+                        Console.WriteLine("Ошибка! Операция не распознана!");
+                        break;
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Задачи по C-sharp/Сравнение двух чисел/Сравнение.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Необходимо написать консольную программу, 
+ * где пользователь будет вводить с клавиатуры 2 числа. 
+ * Числа будут сравниваться с последующим выводом в консоль результата этого сравнения 
+ * (равны ли значения, а если нет, то какое число больше/меньше).
+ */
+namespace Сравнение_двух_чисел
+{
+    internal class Сравнение
+    {
+        static void Main()
+        {
+            Console.Write("Введите первое число:  ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите второе число:  ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                Console.WriteLine("Первое число больше второго");
+            }
+            if (num1 < num2)
+            {
+                Console.WriteLine("Первое число меньше второго");
+            }
+            if (num1 == num2)
+            {
+                Console.WriteLine("Оба числа равны");
+            }
+        }
+    }
+}
