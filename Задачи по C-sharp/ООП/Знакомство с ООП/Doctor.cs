301cebdd6e57fe5962bb6840b58e56f834fed048
Val16eria <85391921+Val16eria@users.noreply.github.com>
2022-04-17 00:43:27 +0300

    Sharp

---
+++ Задачи по C-sharp/ООП/Знакомство с ООП/Doctor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Знакомство_с_ОПП
+{
+    public class Doctor : Знакомство_с_ООП.Person
+    {
+        public string Specialization;
+    }
+}
