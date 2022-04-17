94b7a6aeaf61278b6208cbaa134b8ce0c1665c95
Val16eria <85391921+Val16eria@users.noreply.github.com>
2022-04-17 18:13:23 +0300

    Sharp

---
+++ Задачи по C-sharp/ООП/Знакомство с ООП/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Знакомство_с_ООП
+{
+    public class Person
+    {
+        public string SecondName { get; set; }
+
+        public string Name { get; set; }
+
+        public Person(string secondName, string name)
+        {
+            SecondName = secondName;
+            Name = name;
+        }
+    }
+}
---
+++ Задачи по C-sharp/ООП/Знакомство с ООП/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Знакомство_с_ООП
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = Console.ReadLine();
+            Person p = new Person("Васев", "Вася");
+
+            Console.WriteLine(p.Name);
+            Console.WriteLine(p.SecondName); 
+            Console.ReadLine(); 
+        }
+    }
+}
