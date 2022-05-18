c4ecdc5422f35c3d1300001a4d7df24a39e10169
Val16eria <85391921+Val16eria@users.noreply.github.com>
2022-05-18 22:18:15 +0300

    Sharp

---
+++ Xamarin/StLayoutApp/StLayoutApp/StLayoutApp/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace StLayoutApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            Content = new StackLayout
+            {
+                Orientation = StackOrientation.Horizontal,
+                Children =
+                {
+                    new Label { Text = "Основные цвета" },
+                    new BoxView { Color = Color.Red },
+                    new BoxView { Color = Color.Yellow },
+                    new BoxView { Color = Color.Blue },
+                    new Label { Text = "Основные цвета" },
+                    new BoxView { Color = Color.Green },
+                    new BoxView { Color = Color.Orange },
+                    new BoxView { Color = Color.Purple },
+                }
+            };           
+        }
+    }
+}
